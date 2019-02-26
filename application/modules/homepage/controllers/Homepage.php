9094579c78712be07a66dfb455a3d3cf3acfd9f2
alfthakbr <kyangbesar@gmail.com>
2019-02-27 00:42:19 +0700

    a

--- application/modules/homepage/controllers/Homepage.php
+++ application/modules/homepage/controllers/Homepage.php
@@ -1,21 +1,15 @@
-<?php 
-	
-	if (! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-	class Homepage extends CI_Controller
+class Homepage extends CI_Controller {
+
+	public function index()
 	{
 
-		public function index()
-		{
+		$data['title'] = 'Homepage';
 
-			$data['title'] = 'Dashboard';
-
-			$this->load->modules('templates/header', $data);
-			$this->load->view('homepage/view_homepage');
-
-		}
-
+		$this->load->view('templates/header', $data);
+		$this->load->view('view_homepage');
+		$this->load->view('templates/footer');
 	}
-
-
- ?>
+}
